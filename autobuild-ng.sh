16de636b12a1d94c144ae6abfed6417379b50876
Ella Stanforth <ella@iglunix.org>
2023-03-27 10:44:35 +0100

    fix syntax issue

--- autobuild-ng.sh
+++ autobuild-ng.sh
@@ -10,7 +10,7 @@ if [ ! -z "$GITHUB_TOKEN" ]; then
 	./fetch_latest.sh
 fi
 
-if [ ! -d "./sysroot"]; then
+if [ ! -d "./sysroot" ]; then
 	printf '%s\n' 'Must provide ./sysroot or provide $GITHUB_TOKEN to download one!'
 fi
 
