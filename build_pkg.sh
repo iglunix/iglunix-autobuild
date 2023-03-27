18cb217c72247fbd6533dcc0f52858b22cfb6c72
Ella Stanforth <ella@iglunix.org>
2023-03-27 19:01:38 +0100

    update build_pkg.sh to install to sysroot

--- build_pkg.sh
+++ build_pkg.sh
@@ -7,3 +7,6 @@ printf 'I should be building package: %s\n' $1
 cd /build/iglunix/$1
 
 /build/iglupkg/iglupkg.sh bp
+
+tar -xf out/$(basename "$1").*.tar -C /
+printf 'Done building package: %s\n' $1
