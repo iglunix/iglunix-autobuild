98c42cc3ea428e5c8e570780d6621a3ec36ce4f6
Ella Stanforth <ella@iglunix.org>
2023-03-30 05:20:18 +0100

    only output logs if there is an error

--- build_pkg.sh
+++ build_pkg.sh
@@ -6,7 +6,11 @@ printf 'I should be building package: %s\n' $1
 
 cd /build/iglunix/$1
 
-/build/iglupkg/iglupkg.sh bp
+LOG=$(mktemp)
+
+if ! /build/iglupkg/iglupkg.sh bp > $LOG; then
+	cat $LOG
+fi
 
 tar -tf out/$(basename "$1").*.tar
 tar -xf out/$(basename "$1").*.tar -C /
